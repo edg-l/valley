131fb4513efd1d8a3162471067372683b020528a
Edgar <git@edgarluque.com>
2024-09-30 12:08:12 +0200

    Create README.md

---
+++ README.md
@@ -0,0 +1,110 @@
+# Valley
+
+A sierra decompiler into a "verbose" version of Cairo.
+
+The decompiled code is not valid cairo but it may help in understanding the original code.
+
+> [!IMPORTANT]
+> Currently in very very very early development, in my free time.
+
+# Example
+```cairo
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+
+pub fn sub(a: u32, b: u32, c: u32) -> u32 {
+    a - b - c
+}
+```
+
+```
+valley program.sierra
+```
+
+Generates:
+
+```cairo
+
+pub fn func_10915670729824419620(v0: RangeCheck, v1: u32, v2: u32) -> (RangeCheck, Enum<(u32), ((), Array<felt252>)>) {
+    let (v6 : u32, v6_overflowed: bool) = v1 + v2;
+    if v6_overflowed {
+        let v3: RangeCheck = v0;
+        let v7: (u32) = Struct {
+        	field_0: v4,
+        };
+        let v8: Enum<(u32), ((), Array<felt252>)> = Enum::Variant0(v7);
+
+        return v3, v8;
+    } else {
+        let v5: RangeCheck = v0;
+        drop(v6);
+        let mut v9: Array<felt252> = Array::new();
+        let v10: felt252 = 155785504323917466144735657540098748279;
+        v9.append(v10);
+        let mut v11: Array<felt252> = v9;
+        let v12: () = Struct {
+        };
+        let v13: ((), Array<felt252>) = Struct {
+        	field_0: v12,
+        	field_1: v11,
+        };
+        let v14: Enum<(u32), ((), Array<felt252>)> = Enum::Variant1(v13);
+
+        return v5, v14;
+    }
+}
+
+pub fn func_2293170801303492997(v0: RangeCheck, v1: u32, v2: u32, v3: u32) -> (RangeCheck, Enum<(u32), ((), Array<felt252>)>) {
+    let (v7 : u32, v7_overflowed: bool) = v1 - v2;
+    if v7_overflowed {
+        let v4: RangeCheck = v0;
+        let (v11 : u32, v11_overflowed: bool) = v5 - v3;
+        if v11_overflowed {
+            let v8: RangeCheck = v4;
+            let v12: (u32) = Struct {
+            	field_0: v9,
+            };
+            let v13: Enum<(u32), ((), Array<felt252>)> = Enum::Variant0(v12);
+
+            return v8, v13;
+        } else {
+            let v10: RangeCheck = v4;
+            drop(v11);
+            let mut v14: Array<felt252> = Array::new();
+            let v15: felt252 = 155785504329508738615720351733824384887;
+            v14.append(v15);
+            let mut v16: Array<felt252> = v14;
+            let v17: () = Struct {
+            };
+            let v18: ((), Array<felt252>) = Struct {
+            	field_0: v17,
+            	field_1: v16,
+            };
+            let v19: Enum<(u32), ((), Array<felt252>)> = Enum::Variant1(v18);
+
+            return v10, v19;
+        }
+    } else {
+        let v6: RangeCheck = v0;
+        drop(v7);
+        drop(v3);
+        let mut v20: Array<felt252> = Array::new();
+        let v21: felt252 = 155785504329508738615720351733824384887;
+        v20.append(v21);
+        let mut v22: Array<felt252> = v20;
+        let v23: () = Struct {
+        };
+        let v24: ((), Array<felt252>) = Struct {
+        	field_0: v23,
+        	field_1: v22,
+        };
+        let v25: Enum<(u32), ((), Array<felt252>)> = Enum::Variant1(v24);
+
+        return v6, v25;
+    }
+}
+
+
+```
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, error::Error, path::PathBuf};
 use cairo_lang_sierra::{
     extensions::{
         array::ArrayConcreteLibfunc,
+        const_type::ConstConcreteLibfunc,
         core::{CoreConcreteLibfunc, CoreLibfunc, CoreType, CoreTypeConcrete},
         enm::EnumConcreteLibfunc,
         gas::GasConcreteLibfunc,
@@ -105,7 +106,7 @@ pub fn build_statement(
     depth_level: usize,
 ) -> Result<(), Box<dyn Error>> {
     let st = &statements[statement_idx.0];
-    let tabs: String = "\t".chars().cycle().take(depth_level).collect();
+    let tabs: String = " ".chars().cycle().take(depth_level * 4).collect();
 
     match st {
         GenStatement::Invocation(gen_invocation) => {
@@ -129,7 +130,7 @@ pub fn build_statement(
                         let out_ty = &info.branch_signatures()[0].vars[0].ty;
 
                         buffunc.push_str(&format!(
-                            "{tabs}let v{:?}: {} = Array::new();\n",
+                            "{tabs}let mut v{:?}: {} = Array::new();\n",
                             outvarid.id,
                             get_type_name(reg, out_ty)?
                         ));
@@ -140,7 +141,26 @@ pub fn build_statement(
                     }
                     ArrayConcreteLibfunc::SpanFromTuple(_) => todo!(),
                     ArrayConcreteLibfunc::TupleFromSpan(_) => todo!(),
-                    ArrayConcreteLibfunc::Append(_) => todo!(),
+                    ArrayConcreteLibfunc::Append(info) => {
+                        let outvarid = &gen_invocation.branches[0].results[0];
+                        let out_ty = &info.branch_signatures()[0].vars[0].ty;
+
+                        buffunc.push_str(&format!(
+                            "{tabs}v{:?}.append(v{:?});\n",
+                            gen_invocation.args[0].id, gen_invocation.args[1].id,
+                        ));
+
+                        buffunc.push_str(&format!(
+                            "{tabs}let mut v{:?}: {} = v{:?};\n",
+                            outvarid.id,
+                            get_type_name(reg, out_ty)?,
+                            gen_invocation.args[0].id,
+                        ));
+
+                        assert_eq!(gen_invocation.branches.len(), 1);
+                        let idx = statement_idx.next(&gen_invocation.branches[0].target);
+                        build_statement(buffunc, reg, idx, statements, varids, depth_level)?;
+                    }
                     ArrayConcreteLibfunc::PopFront(_) => todo!(),
                     ArrayConcreteLibfunc::PopFrontConsume(_) => todo!(),
                     ArrayConcreteLibfunc::Get(_) => todo!(),
@@ -175,12 +195,43 @@ pub fn build_statement(
                 }
                 CoreConcreteLibfunc::Ec(_) => todo!(),
                 CoreConcreteLibfunc::Felt252(_) => todo!(),
-                CoreConcreteLibfunc::Const(info) => {
-                    let outvarid = &gen_invocation.branches[0].results[0];
-                    let out_ty = get_type_name(reg, &info.branch_signatures()[0].vars[0].ty)?;
+                CoreConcreteLibfunc::Const(selector) => match selector {
+                    ConstConcreteLibfunc::AsBox(_) => todo!(),
+                    ConstConcreteLibfunc::AsImmediate(info) => {
+                        let outvarid = &gen_invocation.branches[0].results[0];
+                        let out_ty = get_type_name(reg, &info.branch_signatures()[0].vars[0].ty)?;
+                        let const_type = reg.get_type(&info.const_type)?;
 
-                    buffunc.push_str(&format!("{tabs}let v{:?} = {out_ty};\n", outvarid.id,));
-                }
+                        let out_val = if let CoreTypeConcrete::Const(info) = const_type {
+                            let mut data = String::new();
+
+                            for x in &info.inner_data {
+                                match x {
+                                    GenericArg::UserType(_user_type_id) => todo!(),
+                                    GenericArg::Type(_concrete_type_id) => todo!(),
+                                    GenericArg::Value(big_int) => {
+                                        data.push_str(&big_int.to_str_radix(10))
+                                    }
+                                    GenericArg::UserFunc(_function_id) => todo!(),
+                                    GenericArg::Libfunc(_concrete_libf_unc_id) => todo!(),
+                                }
+                            }
+
+                            data
+                        } else {
+                            unreachable!()
+                        };
+
+                        buffunc.push_str(&format!(
+                            "{tabs}let v{:?}: {out_ty} = {out_val};\n",
+                            outvarid.id
+                        ));
+
+                        assert_eq!(gen_invocation.branches.len(), 1);
+                        let idx = statement_idx.next(&gen_invocation.branches[0].target);
+                        build_statement(buffunc, reg, idx, statements, varids, depth_level)?;
+                    }
+                },
                 CoreConcreteLibfunc::FunctionCall(_) => todo!(),
                 CoreConcreteLibfunc::Gas(selector) => match selector {
                     GasConcreteLibfunc::WithdrawGas(_) => {}
@@ -219,7 +270,7 @@ pub fn build_statement(
                             let range_check_id = gen_invocation.branches[0].results[0].id;
                             let out_ty = &info.branch_signatures()[0].vars[0].ty;
                             buffunc.push_str(&format!(
-                                "{tabs}\tlet v{}: {} = v{:?};\n",
+                                "{tabs}    let v{}: {} = v{:?};\n",
                                 range_check_id,
                                 get_type_name(reg, out_ty)?,
                                 gen_invocation.args[0].id
@@ -236,7 +287,7 @@ pub fn build_statement(
                             let range_check_id = gen_invocation.branches[1].results[0].id;
                             let out_ty = &info.branch_signatures()[1].vars[0].ty;
                             buffunc.push_str(&format!(
-                                "{tabs}\tlet v{}: {} = v{:?};\n",
+                                "{tabs}    let v{}: {} = v{:?};\n",
                                 range_check_id,
                                 get_type_name(reg, out_ty)?,
                                 gen_invocation.args[0].id
